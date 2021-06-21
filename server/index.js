d436f312abeca8622bea6b130faa9f113c565e94
Doriane <berdalado@gmail.com>
2021-06-21 15:00:49 +0200

    chat websocket

--- server/index.js
+++ server/index.js
@@ -1,1 +1,33 @@
 // TODO: write server code here
+//ecoute les connection et les non connections
+
+const express = require("express");
+const uniqid = require("uniqid");
+const app = express();
+const socketIO = require("socket.io");
+
+const server = app.listen(3001);
+
+const io = socketIO(server, {
+  cors: { origin: ["http://localhost:3002"] },
+});
+
+const messages = [
+  { id: uniqid(), author: "server", text: "welcome to WildChat" },
+];
+
+//ecouter connection a la web sockets : notre serveur web socket, quand client se connecte il execute une fonction
+io.on("connect", (socket) => {
+  console.log("user connected");
+  socket.on("disconnect", () => {
+    //recupere l'objet socket, si détecte évt deconnexion
+    console.log("user disconnected");
+  });
+  socket.emit("initialMessageList", messages);
+
+  socket.on("messageFromClient", (newMessage) => {
+    const toSend = { id: uniqid(), ...newMessage };
+    messages.push(toSend); //push un nouvel objet, id et données provenant du client
+    io.emit("messageFromServer", toSend); //Once the new message is added to the list, we have to send it back to (all*) the clients
+  });
+});
--- src/App.js
+++ src/App.js
@@ -1,45 +1,71 @@
-import React, {useState} from 'react'
+import React, { useEffect, useState } from "react";
+import socketIOClient from "socket.io-client"; // On top of App.js :
 
 function App() {
-  const [messageList, setMessageList] = useState([])
-  const [nickName, setNickName] = useState('')
-  const [newMessageText, setNewMessageText] = useState('')
-  const [socket, setSocket] = useState(null)
+  const [messageList, setMessageList] = useState([]);
+  const [nickName, setNickName] = useState("");
+  const [newMessageText, setNewMessageText] = useState("");
+  const [socket, setSocket] = useState(null);
 
-  const handleSubmit = e => {
-    e.preventDefault()
-  }
+  // When the component mounts :
+  // TODO: set the socket in the state to use it later
+  useEffect(() => {
+    const s = socketIOClient("http://localhost:3001");
+    setSocket(s);
+
+    s.on("initialMessageList", (messages) => {
+      setMessageList(messages);
+    });
+
+    s.on("messageFromServer", (newMessage) => {
+      setMessageList((currentMessageList) => [
+        ...currentMessageList,
+        newMessage,
+      ]);
+    });
+    return () => {
+      s.disconnect();
+    };
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    socket.emit("messageFromClient", {
+      text: newMessageText,
+      author: nickName,
+    });
+  };
 
   return (
-    <div className="App">
+    <div className='App'>
       <h2>Messages</h2>
-      {messageList.map(message => {
+      {messageList.map((message) => {
         return (
           <div key={message.id}>
             {message.author} : {message.text}
           </div>
-        )
+        );
       })}
 
       <form onSubmit={handleSubmit}>
         <h2>New Message</h2>
-        <input 
-          type="text"
-          name="author"
-          placeholder="nickname"
+        <input
+          type='text'
+          name='author'
+          placeholder='nickname'
           value={nickName}
           required
           onChange={(e) => setNickName(e.target.value)}
         />
-        <input 
-          type="text"
-          name="messageContent"
-          placeholder="message"
+        <input
+          type='text'
+          name='messageContent'
+          placeholder='message'
           value={newMessageText}
           required
           onChange={(e) => setNewMessageText(e.target.value)}
         />
-        <input type="submit" value="send" />
+        <input type='submit' value='send' />
       </form>
     </div>
   );
